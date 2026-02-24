2e6e4de88e5d636977c2d974d9385de4d08022fb
ankit <ankitkumarswe01@gmail.com>
2026-02-24 10:50:29 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,3 +1,17 @@
+import mongoose from "mongoose"
+const dbConnect = async ()=>{
+    try{
+    await mongoose.connect("mongodb://localhost:27017/merndatabase")
+    console.log("Mongodb connected");
+    }catch(err){
+        console.log("Mongodb not connected");
+    }
+};
+
+export default dbConnect;
+
+
+
 
 
 
